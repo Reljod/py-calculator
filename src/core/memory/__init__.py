7e586bec2adde41a6e906d5edf80d9eb51fe5389
reljod <oretareljod@gmail.com>
2023-07-27 00:20:16 +0800

    feat: create initial abstract class for calculator memory

---
+++ src/core/memory/__init__.py
@@ -0,0 +1,14 @@
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Memory(ABC):
+    
+    @abstractmethod
+    def insert(self, key: str, data: Any) -> list:
+        pass
+    
+    @abstractmethod
+    def pop(self, key: str) -> Any:
+        pass
